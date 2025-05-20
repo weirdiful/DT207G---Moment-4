1e0137d1dfec09c2dce120dd9046779893e35d54
johannahyll <johanna.hyll@gmail.com>
2025-05-20 19:51:14 +0200

    routes bcrypt

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const router = express.Router();
+const sqlite3 = require("sqlite3").verbose();
+const bcrypt = require("bcrypt");
+
+const db = new sqlite3.Database(process.env.DATABASE);
 
 router.post("/register", async (req, res) => {
     try {
@@ -9,7 +14,22 @@ router.post("/register", async (req, res) => {
             return res.status(400).json({ error: "Invalid input, send username and password"});
         }
 
-        res.status(201).json({message: "User created"});
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+        const sql = `INSERT INTO users(username, password) VALUES(?,?)`;
+        db.run(sql, [username, password], (err) => {
+
+             if(err) {
+                res.status(400).json({ message: "Error creating user"});
+
+        } else {
+            res.status(201).json({message: "User created"});
+        }
+
+        });
+
+       
+
     } catch (error) {
         res.status(500).json({ error: "Server error"});
     }
@@ -23,11 +43,22 @@ router.post("/login", async(req, res) => {
             return res.status(400).json({ error: "Invalid input, send username and password"});
         }
 
-        if(username === "name" && password === "password") {
-            res.status(200).json({message: "Login successful"});
+       const sql = `SELECT * FROM users WHERE username=?`;
+       db.get(sql, [username], async (err, row) => {
+        if(err) {
+            res.status(400).json({message: "Error authenticating"});
+        } else if(!row) {
+            res.status(401).json({message: "Incorrect username/password"});
         } else {
-            res.status(401).json({ error: "Invalid username/password"});
-        }
+            const passwordMatch = await bcrypt.compare(password, row.password);
+
+            if(!passwordMatch) {
+                res.status(401).json({message: "Incorrect username/password"});
+            } else {
+                res.status(200).json({message: "Correct login"});
+            }
+         }
+       });
 
        } catch(error) {
         res.status(500).json({ error: "Server error"});
